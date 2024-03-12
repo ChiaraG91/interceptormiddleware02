163d38878c85409091b5947cdd2e5162ca9e7b3e
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-12 15:43:37 +0100

    Add files via upload

---
+++ interceptormiddleware02/src/main/java/com/interceptormiddleware02/interceptormiddleware02/Interceptormiddleware02Application.java
@@ -0,0 +1,13 @@
+package com.interceptormiddleware02.interceptormiddleware02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interceptormiddleware02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Interceptormiddleware02Application.class, args);
+	}
+
+}
---
+++ interceptormiddleware02/src/test/java/com/interceptormiddleware02/interceptormiddleware02/Interceptormiddleware02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interceptormiddleware02.interceptormiddleware02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interceptormiddleware02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
